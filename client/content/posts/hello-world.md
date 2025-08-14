99c0b6168ae813035b2716d3a65bb2118fc83c65
Lem Praew <praewtippong@gmail.com>
2025-08-14 18:22:28 +0700

    Terminal commands changes
    Process to deploy website using TinaCMS

---
+++ client/content/posts/hello-world.md
@@ -0,0 +1,9 @@
+---
+title: "Lem Tippong"
+date: "2025-08-01"
+cover: "/images/hello.jpg"
+---
+
+## This is my first project!
+
+This is my portfolio website project made with Replit and Netlify.
---
+++ client/tina/config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from "tinacms";
+
+const branch =
+  process.env.GITHUB_BRANCH ||
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "main";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.VITE_TINA_CLIENT_ID,
+  token: process.env.TINA_TOKEN,
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "public/uploads",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "projects",
+        label: "Projects",
+        path: "content/projects",
+        fields: [
+          { type: "string", name: "title", label: "Title", isTitle: true, required: true },
+          { type: "datetime", name: "date", label: "Date" },
+          { type: "image", name: "cover", label: "Cover Image" },
+          { type: "rich-text", name: "body", label: "Body", isBody: true }
+        ],
+      },
+    ],
+  },
+});
